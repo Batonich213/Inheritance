075b9fcdfbf1358144842a8b206ed0f956f2ef17
Никита Ботов <botov164@gmail.com>
2023-03-09 23:24:11 +0300

    DONE

---
+++ Academy/main.cpp
@@ -0,0 +1,268 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+#define HUMAN_TAKE_PARAMETERS	const std::string& last_name, const std::string& first_name, int age
+#define HUMAN_GIVE_PARAMETERS	last_name, first_name, age
+class Human
+{
+	std::string last_name;
+	std::string first_name;
+	int age;
+public:
+	const std::string& get_last_name()const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const std::string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const std::string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(int age)
+	{
+		this->age = age;
+	}
+
+	//						Constructors:
+	Human(HUMAN_TAKE_PARAMETERS)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	virtual ~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+
+	virtual void info()const
+	{
+		cout << last_name << " " << first_name << " " << age << endl;
+	}
+	virtual std::ostream& print(std::ostream& os)const
+	{
+		return os << last_name << " " << first_name << " " << age;
+	}
+};
+std::ostream& operator<<(std::ostream& os, const Human& obj)
+{
+	return obj.print(os);
+}
+
+#define STUDENT_TAKE_PARAMETERS	const std::string& speciality, const std::string& group, double rating, double attendance
+#define STUDENT_GIVE_PARAMETERS	speciality, group, rating, attendance
+class Student :public Human
+{
+	std::string speciality;
+	std::string group;
+	double rating;
+	double attendance;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	const std::string& get_group()const
+	{
+		return group;
+	}
+	double get_rating()const
+	{
+		return rating;
+	}
+	double get_attendance()const
+	{
+		return attendance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(double attendance)
+	{
+		this->attendance = attendance;
+	}
+
+	//					Constructors:
+	Student(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "SConstructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestructor:\t" << this << endl;
+	}
+
+	//						Methods:
+	void info()const
+	{
+		Human::info();
+		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
+	}
+	std::ostream& print(std::ostream& os)const override
+	{
+		return Human::print(os) << " " << speciality << " " << group << " " << rating << " " << attendance;
+	}
+};
+
+#define TEACHER_TAKE_PARAMETERS	const std::string& speciality, double experience
+#define TEACHER_GIVE_PARAMETERS	speciality, experience
+class Teacher : public Human
+{
+	std::string speciality;
+	int experience;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	int get_experience()const
+	{
+		return experience;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experiece(double experience)
+	{
+		this->experience = experience;
+	}
+
+	//					Constructors:
+	Teacher(HUMAN_TAKE_PARAMETERS, TEACHER_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
+	{
+		set_speciality(speciality);
+		set_experiece(experience);
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+
+	//				Methods:
+	void info()const
+	{
+		Human::info();
+		cout << speciality << " " << experience << " ëåò.\n";
+	}
+	std::ostream& print(std::ostream& os)const override
+	{
+		return Human::print(os) << " " << speciality << " " << experience;
+	}
+};
+
+#define GRADUATE_TAKE_PARAMETERS	const std::string& subject
+#define GRADUATE_GIVE_PARAMETERS	subject
+class Graduate :public Student
+{
+	std::string subject;
+public:
+	const std::string& get_subject()const
+	{
+		return subject;
+	}
+	void set_subject(const std::string& subject)
+	{
+		this->subject = subject;
+	}
+	Graduate(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS, GRADUATE_TAKE_PARAMETERS)
+		:Student(HUMAN_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS)
+	{
+		set_subject(subject);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+	void info()const
+	{
+		Student::info();
+		cout << subject << endl;
+	}
+	std::ostream& print(std::ostream& os)const override
+	{
+		return Student::print(os) << " " << subject;
+	}
+};
+
+//#define INHERITANCE_CHECK
+#define POLYMORPHISM	//(ìíîãîôîðìåííîñòü)
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef INHERITANCE_CHECK
+	Human human("Òóïåíêî", "Âàñèëèé", 20);
+	human.info();
+
+	Student student("Èâàíîâ", "Èâàí", 18, "IT", "PV_211", 12, 100);
+	student.info();
+
+	Teacher teacher("Einstein", "Albert", 143, "Astronomy", 110);
+	teacher.info();
+
+	Graduate graduate("Ïåòðîâ", "Íèêîëàé", 21, "IT", "PV_211", 10, 80, "Îðãàíèçàöèÿ çàùèòû áåçîïàñíîñòè ÷åòîòàì ñåòè");
+	graduate.info();
+#endif // INHERITANCE_CHECK
+
+	//1. Ad-Hoc polymorphism
+
+	//2. Runtime polymorphism:
+	/*
+		1. Pointers to base class;	//Generalisation - Îáîáùåíèå
+		2. Virtual functions;
+	*/
+
+	//			Generalisation (UpCast)
+	Human* group[] =
+	{
+		new Student("Pinkman", "Jessie", 20, "Chemistry", "WW_220", 89, 92),
+		new Teacher("Whaite", "Walter", 50, "Chemistry", 25),
+		new Graduate("Schreder", "Hank", 40, "Criminalistic", "OBN", 95, 75, "How to catch Heizenberg"),
+		new Student("Vercetti", "Tomas", 30, "Criminalistic", "Vice", 98, 99),
+		new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
+	};
+	cout << sizeof(group) / sizeof(group[0]) << endl;
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		cout << typeid(*group[i]).name() << endl;
+		//group[i]->info();
+		cout << *group[i] << endl;
+		cout << "\n------------------------------------\n";
+	}
+
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		delete group[i];
+	}
+	//delete[] group;	//Ïðè óäàëåíèè ñòàòè÷åñêîé ïàìÿòè âûëåòàåò Debug Assertion Failed
+}
--- Files/main.cpp
+++ Files/main.cpp
@@ -2,17 +2,53 @@
 #include<fstream>
 using namespace std;
 
-#define write_to_file
-#define read_from_file
+//#define WRITE_TO_FILE
+#define READ_FROM_FILE
+
+const char* UNITS[] = { "B", "kB", "MB", "GB" };
+
 void main()
 {
 	setlocale(LC_ALL, "");
-#ifdef write_to_file
-	ofstream fout;
-	fout.open("File.txt", std::ios_base::app);
-	fout << "Hello Files" << endl;
+	cout << "Hello World";
+	cout << endl;
+	//https://legacy.cplusplus.com/doc/tutorial/files/
+
+#ifdef WRITE_TO_FILE
+	ofstream fout;			//1) Создаем поток
+	fout.open("File.txt", std::ios_base::app);	//2) Открываем поток
+	fout << "Hello Files" << endl;//3) Пишем в поток
 	fout << "Всем привет" << endl;
-	fout.close();
+	fout.close();			//4) После того как запись в поток выполнена, его обязательно нужно закрыть!!!
 	system("notepad File.txt");
-#endif 
+#endif // WRITE_TO_FILE
+
+	std::ifstream fin("File.txt");
+	cout << "Начальная позиция курсора: " << fin.tellg() << endl;
+	fin.seekg(0, ios::end);
+	int i = 0;
+	int filesize = fin.tellg();
+	cout << "Конечная позиция курсора: " << filesize << endl;
+	for (; filesize > 1024; i++, filesize /= 1024);
+	cout << "Размер файла: " << filesize << " " << UNITS[i] << endl;
+	//fin.tellg() - tell get position (говорит позцию считывающего курсора)
+	system("PAUSE");
+	if (fin.is_open())
+	{
+		//TODO: будем читать из файла
+		const int SIZE = 1024 * 512;
+		char sz_buffer[SIZE] = {};
+		while (!fin.eof())
+		{
+			//fin >> sz_buffer;
+			fin.getline(sz_buffer, SIZE);	//getline() позволяет ввести строку с пробелами, до заданного разделителя
+			//по умолчанию, разделителем для fin.getline() является '\n'
+			cout << sz_buffer << endl;
+		}
+		fin.close();
+	}
+	else
+	{
+		std::cerr << "Error: file not found" << endl;
+	}
 }
