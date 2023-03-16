1a75784a73772655e1b160a852cd9133e220c426
Никита Ботов <botov164@gmail.com>
2023-03-16 03:28:26 +0300

    ... Bullshit

---
+++ AbstractBaseClass/main.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+
+class Vehicle	
+{
+	int speed;
+public:
+	virtual void move() = 0;	
+};
+
+class GroundVehicle :public Vehicle {};
+
+class Car :public GroundVehicle
+{
+public:
+	void move()override
+	{
+		cout << "Ìàøèíà åäèò íà ÷åòûðåõ êîëåñàõ, ñî ñêîðîñòüþ " << endl;
+	}
+};
+class Bike :public GroundVehicle
+{
+public:
+	void move()override
+	{
+		cout << "Ìîòîöèêë åäèò íà äâóõ êîëåñàõ" << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//Vehicle vehicle;
+	//GroundVehicle gv;
+	Car bmw;
+	bmw.move();
+
+	Bike harley_devidson;
+	harley_devidson.move();
+}
